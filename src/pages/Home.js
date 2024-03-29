e334be125066336fb0407718a048fd3efae510c0
--global <frontend.divine@gmail.com>
2024-03-29 20:44:24 +0530

    my commite

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "../Component/Nav";
+import Word from "../Component/Word";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Word />
+    </>
+  );
+}
+
+export default Home;
